10ee34f06fc039bc9336b70d22d39c0c2393ba04
miansen <1158827539@qq.com>
2019-06-12 17:13:44 +0800

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function () {
 
   // 替换代码块中 p 标签的 class
-  $("p.err").toggleClass("p");
+  $(".highlighter-rouge span.err").toggleClass("p");
 
   $(".fa-chevron-right").each(function(index){
     if (index === 1) {
